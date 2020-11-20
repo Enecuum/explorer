fc3648971852e6da072398ee6b24216149d870b5
@djdfy <vtsarev@enecuum.com>
2020-11-20 18:39:27 +0300

    [NT-481] add token customer info

--- assets/info/pos-info-storage-enq.js
+++ assets/info/pos-info-storage-enq.js
@@ -1,21 +1,21 @@
 (function() {
 	let POSInfoStorage = [
 		{
-			posId: '018f23b3fd60c28e12bd428296b98250394b44839d438b5022a9a20686ebd99d',
+			posId: 'some-staking-provider-id',
 			webSite: 'https://somePOSWebSite.com',
 			media: {                            
-                    facebook: 'https://www.facebook.com/some-staking-provider',
-                    twitter: 'https://twitter.com/some-staking-provider',
-                    medium: 'https://medium.com/some-staking-provider',
-                    github: 'https://github.com/some-staking-provider',
-                    forklog: 'https://hub.forklog.com/some-staking-provider',
-                    bitcointalk: 'https://bitcointalk.org/some-staking-provider',
-                    linkedin: 'https://www.linkedin.com/some-staking-provider',
-                    youtube: 'https://www.youtube.com/some-staking-provider',
-                    qq: 'https://qm.qq.com/some-staking-provider',
-                    weixin: 'https://weixin.qq.com/some-staking-provider',
-                    telegram: 'https://t.me/some-staking-provider'
-                },
+                facebook: 'https://www.facebook.com/some-staking-provider',
+                twitter: 'https://twitter.com/some-staking-provider',
+                medium: 'https://medium.com/some-staking-provider',
+                github: 'https://github.com/some-staking-provider',
+                forklog: 'https://hub.forklog.com/some-staking-provider',
+                bitcointalk: 'https://bitcointalk.org/some-staking-provider',
+                linkedin: 'https://www.linkedin.com/some-staking-provider',
+                youtube: 'https://www.youtube.com/some-staking-provider',
+                qq: 'https://qm.qq.com/some-staking-provider',
+                weixin: 'https://weixin.qq.com/some-staking-provider',
+                telegram: 'https://t.me/some-staking-provider'
+            },
             descriptionLocaleStringArray: {
                 en: 'EN text about some Staking provider',
             	de: 'DE text about some Staking provider',
@@ -31,35 +31,35 @@
 			posId: 'another-staking-provider-id',
 			webSite: 'https://anotherPOSWebSite.com',
             media: {                            
-                    facebook: 'https://www.facebook.com/another-staking-provider',
-                    twitter: 'https://twitter.com/another-staking-provider',
-                    medium: 'https://medium.com/another-staking-provider',
-                    github: 'https://github.com/another-staking-provider',
-                    forklog: 'https://hub.forklog.com/another-staking-provider',
-                    bitcointalk: 'https://bitcointalk.org/another-staking-provider',
-                    linkedin: 'https://www.linkedin.com/another-staking-provider',
-                    youtube: 'https://www.youtube.com/another-staking-provider',
-                    qq: 'https://qm.qq.com/another-staking-provider',
-                    weixin: 'https://weixin.qq.com/another-staking-provider',
-                    telegram: 'https://t.me/another-staking-provider'
-                },
+                facebook: 'https://www.facebook.com/another-staking-provider',
+                twitter: 'https://twitter.com/another-staking-provider',
+                medium: 'https://medium.com/another-staking-provider',
+                github: 'https://github.com/another-staking-provider',
+                forklog: 'https://hub.forklog.com/another-staking-provider',
+                bitcointalk: 'https://bitcointalk.org/another-staking-provider',
+                linkedin: 'https://www.linkedin.com/another-staking-provider',
+                youtube: 'https://www.youtube.com/another-staking-provider',
+                qq: 'https://qm.qq.com/another-staking-provider',
+                weixin: 'https://weixin.qq.com/another-staking-provider',
+                telegram: 'https://t.me/another-staking-provider'
+            },
             descriptionLocaleStringArray: {
                 en: 'EN text about another Staking provider',
                 de: 'DE text about another Staking provider'
             }             
 		},
-    {
-      posId: '0dc30e84a6be4dff923a84c7511006e195531817c5b54650a42f6fcdc6ebc253',
-      webSite: 'https://genesislab.net',
-      media: {
-        twitter: 'https://twitter.com/genesislab_net',
-        github: 'https://github.com/genesis-lab-team',
-        telegram: 'https://t.me/GenesisLab_EN'
-      },
-      descriptionLocaleStringArray: {
-        en: 'Genesis Lab is a blockchain-focused development company and trusted Proof-of-Stake validator to comfortably stake your coins and earn rewards with Enecuum, Near, Cosmos, Polkadot, IRIS, Kusama, Kava, Terra and other networks.'
-      }
-    }
+        {
+            posId: '0dc30e84a6be4dff923a84c7511006e195531817c5b54650a42f6fcdc6ebc253',
+            webSite: 'https://genesislab.net',
+            media: {
+                twitter: 'https://twitter.com/genesislab_net',
+                github: 'https://github.com/genesis-lab-team',
+                telegram: 'https://t.me/GenesisLab_EN'
+            },
+            descriptionLocaleStringArray: {
+                en: 'Genesis Lab is a blockchain-focused development company and trusted Proof-of-Stake validator to comfortably stake your coins and earn rewards with Enecuum, Near, Cosmos, Polkadot, IRIS, Kusama, Kava, Terra and other networks.'
+            }
+        }
 	];
 	window.POSInfoStorageENQ = POSInfoStorage;
 })();
---
+++ assets/info/token-info-storage-bit.js
@@ -0,0 +1,83 @@
+(function() {
+	let tokenInfoStorage = [
+		{
+			tokenId: '0000000000000000000000000000000000000000000000000000000000000000',
+			webSite: 'https://enecuum.com',
+			media: {                            
+                facebook: 'https://www.facebook.com/enecuum.EN/?ref=bookmarks',
+                twitter: 'https://twitter.com/enq_enecuum',
+                medium: 'https://medium.com/@EnqBlockchain',
+                github: 'https://github.com/Enecuum',
+                forklog: 'https://hub.forklog.com/companies/enecuum/',
+                bitcointalk: 'https://bitcointalk.org/index.php?topic=2939909.0;topicseen',
+                linkedin: 'https://www.linkedin.com/company/enecuum-limited/',
+                youtube: 'https://www.youtube.com/channel/UCyZqNfzK_PP82nkAVOlmN4Q',
+                qq: 'https://qm.qq.com/cgi-bin/qm/qr?k=YfdokyjJnlKl0eAuhVXEc6JtVsoKnxvI&authKey=6WpHwP4Cu/kAAyPyVuBWZeQCjhEyijvLyGf6Jj6P7f9LVCe/9ZZC7YIfRtudjD23&noverify=0',
+                weixin: 'https://weixin.qq.com/g/AwYAAJdSThUYHIJmetSinRGfcwtrkRovBmLZZr5moi0UXRIW56Me4ts56gByaeYU',
+                telegram: 'https://t.me/Enecuum_EN'
+                },
+            documents: {
+                whitepaper: 'https://enecuum.com/pdf/pp_en.pdf'
+            },     
+            descriptionLocaleStringArray: {
+                en: "BIT is not intended for trading; its sole purpose is testing of Enecuum's Trinity protocol: a combination of Proof-of-Activity, Proof-of-Work, and Proof-of-Stake algorithms. By launching BIT, Enecuum allows users to experience mobile mining without buying minimal stake and to try out PoW and PoS nodes before the Mainnet release with a tradable ENQ.",
+            	de: "BIT is not intended for trading; its sole purpose is testing of Enecuum's Trinity protocol: a combination of Proof-of-Activity, Proof-of-Work, and Proof-of-Stake algorithms. By launching BIT, Enecuum allows users to experience mobile mining without buying minimal stake and to try out PoW and PoS nodes before the Mainnet release with a tradable ENQ.",
+                fr: "BIT is not intended for trading; its sole purpose is testing of Enecuum's Trinity protocol: a combination of Proof-of-Activity, Proof-of-Work, and Proof-of-Stake algorithms. By launching BIT, Enecuum allows users to experience mobile mining without buying minimal stake and to try out PoW and PoS nodes before the Mainnet release with a tradable ENQ.",
+                ru: "BIT is not intended for trading; its sole purpose is testing of Enecuum's Trinity protocol: a combination of Proof-of-Activity, Proof-of-Work, and Proof-of-Stake algorithms. By launching BIT, Enecuum allows users to experience mobile mining without buying minimal stake and to try out PoW and PoS nodes before the Mainnet release with a tradable ENQ.",
+                pt: "BIT is not intended for trading; its sole purpose is testing of Enecuum's Trinity protocol: a combination of Proof-of-Activity, Proof-of-Work, and Proof-of-Stake algorithms. By launching BIT, Enecuum allows users to experience mobile mining without buying minimal stake and to try out PoW and PoS nodes before the Mainnet release with a tradable ENQ.",
+                vi: "BIT is not intended for trading; its sole purpose is testing of Enecuum's Trinity protocol: a combination of Proof-of-Activity, Proof-of-Work, and Proof-of-Stake algorithms. By launching BIT, Enecuum allows users to experience mobile mining without buying minimal stake and to try out PoW and PoS nodes before the Mainnet release with a tradable ENQ.",
+                in: "BIT is not intended for trading; its sole purpose is testing of Enecuum's Trinity protocol: a combination of Proof-of-Activity, Proof-of-Work, and Proof-of-Stake algorithms. By launching BIT, Enecuum allows users to experience mobile mining without buying minimal stake and to try out PoW and PoS nodes before the Mainnet release with a tradable ENQ.",
+                tr: "BIT is not intended for trading; its sole purpose is testing of Enecuum's Trinity protocol: a combination of Proof-of-Activity, Proof-of-Work, and Proof-of-Stake algorithms. By launching BIT, Enecuum allows users to experience mobile mining without buying minimal stake and to try out PoW and PoS nodes before the Mainnet release with a tradable ENQ."
+            }             
+		},
+        {
+            tokenId: '5055012074899d433d9329c35bf26ecf6dcf17304522dbebcfc8e479ec23a3c4',
+            webSite: 'https://someTokenWebSite.com',
+            media: {                            
+                facebook: 'https://www.facebook.com/some-token',
+                twitter: 'https://twitter.com/some-token',
+                medium: 'https://medium.com/some-token',
+                github: 'https://github.com/some-token',
+                forklog: 'https://hub.forklog.com/some-token',
+                bitcointalk: 'https://bitcointalk.org/some-token',
+                linkedin: 'https://www.linkedin.com/some-token',
+                youtube: 'https://www.youtube.com/some-token',
+                qq: 'https://qm.qq.com/some-token',
+                weixin: 'https://weixin.qq.com/some-token',
+                telegram: 'https://t.me/some-token'
+            },
+            descriptionLocaleStringArray: {
+                en: 'EN text about some token',
+                de: 'DE text about some token',
+                fr: 'FR text about some token',
+                ru: 'RU text about some token',
+                pt: 'PT text about some token',
+                vi: 'VI text about some token',
+                in: 'IN text about some token',
+                tr: 'TR text about some token'
+            }             
+        },
+        {
+            tokenId: 'another-token-id',
+            webSite: 'https://anotherPOSWebSite.com',
+            media: {                            
+                facebook: 'https://www.facebook.com/another-token',
+                twitter: 'https://twitter.com/another-token',
+                medium: 'https://medium.com/another-token',
+                github: 'https://github.com/another-token',
+                forklog: 'https://hub.forklog.com/another-token',
+                bitcointalk: 'https://bitcointalk.org/another-token',
+                linkedin: 'https://www.linkedin.com/another-token',
+                youtube: 'https://www.youtube.com/another-token',
+                qq: 'https://qm.qq.com/another-token',
+                weixin: 'https://weixin.qq.com/another-token',
+                telegram: 'https://t.me/another-token'
+            },
+            descriptionLocaleStringArray: {
+                en: 'EN text about another token',
+                de: 'DE text about another token'
+            }             
+        }
+	];
+	window.tokenInfoStorageBIT = tokenInfoStorage;
+})();
